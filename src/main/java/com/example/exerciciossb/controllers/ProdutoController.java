95ba77118848e5faa94174c616cf460133a68ac8
LucasAKuhn <lucasa.kuhn@hotmail.com>
2022-07-01 18:11:54 -0300

    finalizado Spring Boot

--- src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,16 @@ public class ProdutoController {
 	@GetMapping(path="/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
+	}
+	
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
 	}
 }
